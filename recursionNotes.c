1e29b660b76ccdf4181fb30efbcabd8035617244
dylan <dylanwilkins2308@gmail.com>
2024-09-30 16:00:08 -0400

    added more unwinds

--- recursionNotes.c
+++ recursionNotes.c
@@ -29,6 +29,14 @@ void reverseString(char string[], int length){
     printf("%c", string[length-1]); /*printing first because we want to start at the end*/
     reverseString(string, length-1); 
 }
+/*  Unwinding the recursive calls
+    prints(string[length(5)-1], so 'o')
+    calls the function of string with (string, length-1(4))
+    prints(string[length(4)-1, so'l'])
+    calls the function of string with (string, length-1(3))
+
+    This continues until the length is 0, printing 'hello'.  
+*/
 
 int multiply(int first, int second){
     if(second<=0){ /*base case*/
@@ -36,6 +44,10 @@ int multiply(int first, int second){
     }
     return(first+multiply(first, second-1));
 }
+/*  Unwinding the recursive calls
+    Adds the first value over and over until second is 0
+    effectively multiplying the values
+*/
 
 /*TODO
 fast exponent
