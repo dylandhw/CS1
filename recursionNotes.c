61f21aee67dde94f94170c68ca334d9d3129e0c0
dylan <dylanwilkins2308@gmail.com>
2024-09-30 12:25:10 -0400

    multiply with recursion

--- recursionNotes.c
+++ recursionNotes.c
@@ -9,8 +9,23 @@ int factorial(int n){
 
 void reverseString(char string[], int length){
     if(length == 0){
-        return; /*recursive case*/
+        return; /*base case*/
     }
     printf("%c", string[length-1]); /*printing first because we want to start at the end*/
     reverseString(string, length-1); 
 }
+
+int multiply(int first, int second){
+    if(second<=0){ /*base case*/
+        return 0;
+    }
+    return(first+multiply(first, second-1));
+}
+
+/*TODO
+fast exponent
+decimal to binary
+tower of hanoi
+division
+negatives with multiplication
+*/
