1de261fc5fee774d24a0ebef80441beafa33656d
dylan <dylanwilkins2308@gmail.com>
2024-10-01 13:23:14 -0400

    start on some basic structure for PA3

--- ProgrammingAssignment3/main.c
+++ ProgrammingAssignment3/main.c
@@ -1,3 +1,27 @@
-/*Things of note:
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-*/
+#define maxLEN 20
+
+typedef struct {
+    int x, y;
+    char name[maxLEN + 1];
+}Garage;
+
+double calcDistance(Garage gar1, Garage gar2){
+    return sqrt(pow(gar1.x - gar2.x, 2) + pow(gar1.y - gar2.y, 2));
+}
+
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    Garage garages[2*n];
+    for(int i = 0; i < 2*n; i++){
+        scanf("%d %d %s", &garages[i].x, &garages[i].y, garages[i].name);
+    }
+
+    return 0;
+}
