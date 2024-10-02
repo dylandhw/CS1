9ef9d04563fbc9f4b7510e558cdb8356c9b5219a
dylan <dylanwilkins2308@gmail.com>
2024-10-02 16:34:39 -0400

    - nothing important

--- Practice/recursionNuance.c
+++ Practice/recursionNuance.c
@@ -25,6 +25,26 @@ int foo3(int n){
     }
 }
 
+int foo4(int n){
+    if(n == 0){
+        return 1;
+    } else {
+        return (n * foo4(n-1));
+    }
+}
+
+int foo5(int n){
+    if(n <= 1){
+        return n;
+    } else {
+        return (foo5(n-1) + foo5(n-2));
+    }
+}
+
+void foo6(){
+    
+}
+
 int main(){
     int x = 10; 
     printf("!! print before recursive call !!\n");
@@ -36,5 +56,9 @@ int main(){
     int y = foo3(5);
     printf("!! Triangle num of 5 !!\n>>%d<<", y);
     printf("\n---------------------------------\n");
-
+    int a = foo5(5);
+    printf("!! Factorial of 5 !!\n>>%d<<", z);
+    printf("\n---------------------------------\n");
+    printf("!! Factorial of 5 !!\n>>%d<<", z);
+    printf("\n---------------------------------\n");
 }
